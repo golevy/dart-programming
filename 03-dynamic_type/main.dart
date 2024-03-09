44f7810da019657c8fe2481699e4720d2b463f46
Levy <golvwei@gmail.com>
2024-03-09 09:00:00 +0800

    feat: dynamic type

---
+++ 03-dynamic_type/main.dart
@@ -0,0 +1,6 @@
+// The `dynamic` type not only allows the variable's type to change dynamically during runtime but also permits the value to change.
+void main() {
+  dynamic age = 20;
+  age = "Hello";
+  print(age / 3);
+}
